81fffdd88302877914de875bbf2730d05c2afd35
fernando esau amaya alvarado <esauamaya172001>
2021-08-29 16:31:08 -0400

    agregado

--- Formulario MDI/Form3.Designer.cs
+++ Formulario MDI/Form3.Designer.cs
@@ -33,11 +33,13 @@ namespace Formulario_MDI
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
             this.Text = "Ayuda";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
 
         }
--- Formulario MDI/Form3.cs
+++ Formulario MDI/Form3.cs
@@ -16,5 +16,10 @@ namespace Formulario_MDI
         {
             InitializeComponent();
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
