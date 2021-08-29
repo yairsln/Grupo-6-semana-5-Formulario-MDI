0b30f9d419e7d823939718df4f5047d331a28955
KEVIN RODRIGO CORCIO BONILLA <USIS017621@ugb.edu.sv>
2021-08-29 11:14:13 -0600

    correcion

--- Formulario MDI/Form1.cs
+++ Formulario MDI/Form1.cs
@@ -22,17 +22,11 @@ namespace Formulario_MDI
             Form2 f2 = new Form2();
             f2.ShowDialog();
         }
-        //ieuhfioerhcoieruchureihfiuerhfuirhfiuerhfiuerfhuerhieruvhureivheirufvheiruvheriuvheruivheriuvhu//
-        //rjnfierunicuermceriuncievwniweuvniuernviuenviuew//
         private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form3 f3 = new Form3();
             f3.ShowDialog();
         }
-        //nuiwenciunreqiuciuqebciubqe//
-        //niewnciuewqbciwb//
-        //uiewhidubhewiufb//
-        //idewnciuqebcoiu//
         private void cuadroDeDialogoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form4 f4 = new Form4();
