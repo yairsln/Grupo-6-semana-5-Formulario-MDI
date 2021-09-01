beb0ffdb112656d1784c891666f2bbe6b01a0608
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2021-08-31 20:27:31 -0500

    cambios

--- Formulario MDI/Form5.Designer.cs
+++ Formulario MDI/Form5.Designer.cs
@@ -58,6 +58,7 @@ namespace Formulario_MDI
             this.button2.TabIndex = 1;
             this.button2.Text = "Cerrar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // txt_usuario
             // 
