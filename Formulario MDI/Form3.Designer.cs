1b52f363fcc4275c82bb35d37a2c126886e499ff
Gerson Jair Solano Villegas <USIS051921@ugb.edu.sv>
2021-09-05 09:47:29 -0500

    cositas

--- Formulario MDI/Form3.Designer.cs
+++ Formulario MDI/Form3.Designer.cs
@@ -66,6 +66,7 @@ namespace Formulario_MDI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
--- Formulario MDI/Form4.Designer.cs
+++ Formulario MDI/Form4.Designer.cs
@@ -35,6 +35,7 @@ namespace Formulario_MDI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form4";
