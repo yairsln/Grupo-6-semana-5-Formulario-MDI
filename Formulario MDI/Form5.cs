1660d2fd18869008ead10655001160eaf44849f7
KEVIN RODRIGO CORCIO BONILLA <USIS017621@ugb.edu.sv>
2021-09-06 14:08:16 -0600

    correcion de contraseña

--- Formulario MDI/Form5.cs
+++ Formulario MDI/Form5.cs
@@ -20,7 +20,7 @@ namespace Formulario_MDI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txt_usuario.Text == "usuario1" || txt_contraseña.Text == "usuario1")
+            if (txt_usuario.Text == "usuario1" || txt_contraseña.Text == "fernando12345")
             {
 
                 Form1 f1 = new Form1();
