1abcf2b52b0349a031563018921e21184562341b
Genesis Hernández <scarletgenesis2807@gmail.com>
2021-09-06 14:14:00 -0600

    corrrecion de usuario

--- Formulario MDI/Form5.cs
+++ Formulario MDI/Form5.cs
@@ -20,7 +20,7 @@ namespace Formulario_MDI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txt_usuario.Text == "usuario1" || txt_contraseña.Text == "fernando12345")
+            if (txt_usuario.Text == "fernando" || txt_contraseña.Text == "fernando12345")
             {
 
                 Form1 f1 = new Form1();
@@ -29,7 +29,7 @@ namespace Formulario_MDI
             }
             else
                 MessageBox.Show(" Contraseña o nombre incorrecto");
-
+           
                
             
         }
